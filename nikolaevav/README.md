e81fc27eda7776c45074e08b54d9148b8864b824
Nikolaev Artem V <nuriko@inbox.ru>
2012-12-06 21:27:08 +0400

    Исправлены ошибки в тесте nikolaevav. Артем

--- nikolaevav/README.md
+++ nikolaevav/README.md
@@ -1,36 +1,35 @@
 # Output
 
     > ruby matrix.rb
-    Введите конечно число правильной матрицы:
+    Enter the number of the correct matrix:
     1
-    [1]
-
+    1
 
     > ruby matrix.rb
-    Введите конечно число правильной матрицы:
+    Enter the number of the correct matrix:
     4
-    [4, 3]
-    [1, 2]
-
+    4 3
+    1 2
 
     > ruby matrix.rb
-    Введите конечно число правильной матрицы:
+    Enter the number of the correct matrix:
     5
-    Матрица не соответсвует квадрату
+    Matrix is ​​not square
 
     > ruby matrix.rb
-    Введите конечно число правильной матрицы:
+    Enter the number of the correct matrix:
     9
-    [9, 8, 7]
-    [2, 1, 6]
-    [3, 4, 5]
+    9 8 7
+    2 1 6
+    3 4 5
 
     > ruby matrix.rb
-    Введите конечно число правильной матрицы:
+    Enter the number of the correct matrix:
     25
-    [25, 24, 23, 22, 21]
-    [10, 9, 8, 7, 20]
-    [11, 2, 1, 6, 19]
-    [12, 3, 4, 5, 18]
-    [13, 14, 15, 16, 17]
+    25 24 23 22 21
+    10 9  8  7  20
+    11 2  1  6  19
+    12 3  4  5  18
+    13 14 15 16 17
+
 
--- nikolaevav/matrix.rb
+++ nikolaevav/matrix.rb
@@ -1,101 +1,86 @@
 # -*- encoding : utf-8 -*-
 class SMatrix
   def initialize num
-    @min_num = 0
-    @max_num = 1601
     @max_nget = num.to_i
-    #Проверяю число, на валидность
-    if (num == '' or @max_nget <= @min_num or @max_nget >= @max_num)
-      @answer = 'Пожалуйста введите число правильной матрицы от 1 до 1600'
-    else
     #Проверяю на соответсвие квадратной матрицы
-	      x = @min_num
-	      while x <= @max_nget
-		  y = x*x
-		  if y > @max_nget
-		    @answer = 'Матрица не соответсвует квадрату'
-		    @num_col = 0
-		    break
-		  elsif y == num.to_i
-		    @num_col = x
-		    break
-		  else
-		    x +=1
-		  end
-	      end
-    end
+    mth1 = Math.sqrt(@max_nget).to_i
+    mth2 = mth1**2
+      if mth2 == @max_nget
+        @num_col = mth1.to_i
+      else
+        @answer = 'Matrix is ​​not square'
+      end
   end
 
   def create(size)
     x = 0
     Array.new(size) do |r| 
       Array.new(size) do |c| 
-        x = x+1
+        x += 1
       end
     end
   end
 
   def showing
-	if @num_col == 0
-	#Если есть какая-то ошибка в введеном числе, просто выведем сообщение
-	   @answer
-	else
-	#Если определили число столбцов и строк, то заполним массив
-	c,r = @num_col-1,@num_col-1
-	mas = create(@num_col)
+    if @num_col == nil
+      #Если есть какая-то ошибка в введеном числе, просто выведем сообщение
+        puts @answer
+        exit
+    else
+      #Если определили число столбцов и строк, то заполним массив
+      c,r = @num_col-1,@num_col-1
+      mas = create(@num_col)
 
-	i,k,n,x,y = [1,@max_nget,0,0,0]
+      i,k,n,x,y = [1,@max_nget,0,0,0]
+      le = @max_nget.to_s.length
+      while i <= @num_col
+        i += 1
+        #Шаг в право
+	while y < c
+          mas[x][y] = k.to_s.ljust(le)
+	  y += 1
+          k -= 1
+        end
+        #Шаг в низ
+	while x < r
+	  mas[x][y] = k.to_s.ljust(le)
+          x += 1
+          k -= 1
+        end
+        #шаг в лево
+	while y >= n
+	  mas[x][y] = k.to_s.ljust(le)
+	  y -= 1
+	  k -= 1
+        end	
+        #Шаг в вверх
+        y += 1
+        if k == 0
+          break
+        else
+          k += 1
+        end
+        while x > n
+          mas[x][y] = k.to_s.ljust(le)
+            x -= 1
+            k -= 1
+        end
+        c,r = c-1,r-1
+        n,x,y = n+1,x+1,y+1
 
-	   while i <= 4
-	      i += 1
-
-	 	#Шаг в право
-		while y < c
-		 mas[x][y] = k
-		 y += 1
-	         k -= 1
-		end
-		#Шаг в низ
-		while x < r
-		 mas[x][y] = k
-		 x += 1
-	         k -= 1
-		end
-		#шаг в лево
-		while y >= n
-		 mas[x][y] = k
-		 y -= 1
-	         k -= 1
-		end	
-		#Шаг в вверх
-		y += 1
-		if k == 0
-		 break
-		else
-		 k += 1
-		end
-		while x > n
-		mas[x][y] = k
-		 x -= 1
-	         k -= 1
-		end
-		c,r = c-1,r-1
-		n,x,y = n+1,x+1,y+1
-
-		if k == 0
-		 break
-		end
-	   end
-	  
-  	mas.each{|r| p r}
-	puts 
-
+        if k == 0
+          break
 	end
+      end
+      mas.each do |cr|
+        puts "#{cr.map { |r| format(r) }.join(' ')}"
+      end	
+    end
   end
 end
 
-puts 'Введите конечно число правильной матрицы:'
+puts 'Enter the number of the correct matrix:'
 get_a = gets.chomp
 
 think1 = SMatrix.new get_a
-puts think1.showing
+think1.showing
